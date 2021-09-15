b32a67a6f88a9251f669460229a03155b2541504
itay kraise <tatytoy1@gmail.com>
2021-09-15 15:41:55 +0300

    added helper functions

--- shayshayBot.py
+++ shayshayBot.py
@@ -11,36 +11,18 @@ bot = commands.Bot(command_prefix='שישי')
 
 pre = "שישי"
 def getShabat():
-    output = "```css\nStart: "
+    output = "```css"
 
     for event in json.loads(requests.get("https://www.hebcal.com/shabbat?cfg=json;geonameid=294421").text)["items"]:
         if ("title_orig" in event.keys() and event["title_orig"] == "Candle lighting"):
-            #date = daytime.date(2021)
-            startTime = event["date"].split("T")
-            startTime[0] = startTime[0].split("-")
-            startTime[1] = startTime[1].split(":")
-            startTimeObj = datetime.datetime(int(startTime[0][0]), int(startTime[0][1]), int(startTime[0][2]), int(startTime[1][0]), int(startTime[1][1]))
-            output += startTime[1][0] + ":" + startTime[1][1] + "   " + ".".join(startTime[0][:0:-1]) + "\nEnd:   "
+            startTimeObj = extractTime(event)
+            if (startTimeObj.weekday() == 4):  # shabat starts at Friday
+                output += startTimeObj.strftime("\nStart: %H:%M   %d.%m")
         elif ("title_orig" in event.keys() and event["title_orig"] == "Havdalah"):
-            #output += event["title"][event["title"].find(':')+2:] + " " + event["date"][5:10]
-            endTime = event["date"].split("T")
-            endTime[0] = endTime[0].split("-")
-            endTime[1] = endTime[1].split(":")
-            endTimeObj = datetime.datetime(int(endTime[0][0]), int(endTime[0][1]), int(endTime[0][2]), int(endTime[1][0]), int(endTime[1][1]))
-            output += endTime[1][0] + ":" + endTime[1][1] + "   " + ".".join(endTime[0][:0:-1])
-    if (datetime.datetime.now() > endTimeObj):
-        output += "\n\nAvishay is here!!```"
-    elif (datetime.datetime.now() > startTimeObj):
-        diff = endTimeObj - datetime.datetime.now()
-        output += "\n\nTime until avishay comes back:\n"
-        tot = diff.total_seconds()
-        output += str(math.floor(tot)) + " seconds\n" + str(math.floor(tot / 60)) + " minutes\n" + str(math.floor(tot / 60/60)) + " hours\n" + str(math.floor(tot / 60 / 30)) + " average lol games```"
-    else:
-        diff = startTimeObj - datetime.datetime.now() # get diff
-        output += "\n\nTime left with avishay:\n"
-        tot = diff.total_seconds()
-        output += str(math.floor(tot)) + " seconds\n" + str(math.floor(tot / 60)) + " minutes\n" + str(math.floor(tot / 60/60)) + " hours\n" + str(math.floor(tot / 60 / 30)) + " average lol games```"
-    return output
+            endTimeObj = extractTime(event)
+            if (endTimeObj.weekday() == 5):  # shabat ends at Saturday
+                output += endTimeObj.strftime("\nEnd:   %H:%M   %d.%m")
+    return output + prettyPrintTime(startTimeObj, endTimeObj, "avishay comes back", "Avishay is here!!")
 
 def slap():
     return "Slap:wave: <@375656966145703946>"
@@ -53,15 +35,43 @@ def giveCookie():
 def getItay():
     #workEnd = datetime.datetime(18, 0)
     return "Time left with Itay:\n" #, datetime.datetime(18, 0) - datetime.datetime.now()
-    
+ 
 
-#shabatChannel = client.get_channel(863821474951725086)
-#print(shabatChannel)
-#shabatChannel.send('hello')
+def getYomKippur():
+    for event in json.loads(requests.get("https://www.hebcal.com/shabbat?cfg=json;geonameid=294421").text)["items"]:
+        if "title" in event.keys() and event["title"] == "Erev Yom Kippur":
+            print("hu")
+
+    return 1#prettyPrintTime()
+
+
+def extractTime(event):
+    time = event["date"].split("T")
+    time[0] = time[0].split("-")
+    time[1] = time[1].split(":")
+    return datetime.datetime(int(time[0][0]), int(time[0][1]), int(time[0][2]), int(time[1][0]), int(time[1][1]))
+
+def prettyPrintTime(start, end, until, happened):
+    if (datetime.datetime.now() > end):
+        output = "\n\n" + happened + "```"
+    elif (datetime.datetime.now() > start):
+        diff = end - datetime.datetime.now()
+        output = "\n\nTime until" + until + ":\n"
+        tot = diff.total_seconds()
+        output += str(math.floor(tot)) + " seconds\n" + str(math.floor(tot / 60)) + " minutes\n" + str(math.floor(tot / 60/60)) + " hours\n" + str(math.floor(tot / 60 / 30)) + " average lol games```"
+    else:
+        diff = start - datetime.datetime.now() # get diff
+        output = "\n\nTime left with avishay:\n"
+        tot = diff.total_seconds()
+        output += str(math.floor(tot)) + " seconds\n" + str(math.floor(tot / 60)) + " minutes\n" + str(math.floor(tot / 60/60)) + " hours\n" + str(math.floor(tot / 60 / 30)) + " average lol games```"
+    return output
+
+
 commands = {"מתי אבישי": getShabat, "מתי אבישישי": getShabat, "מתי אבשישי": getShabat, "מתי שבת": getShabat,
     "כאפה לאבישי": slap, "כאפה לאבשישי": slap,
     "מי הוא אבישי": avishayHater, "מי הוא אבשישי": avishayHater, "מי אבישי": avishayHater, "מי אבשישי": avishayHater,
-    "מתי איתי": getItay}
+    "מתי איתי": getItay,
+    "שישי אני רעב": getYomKippur, "שישי אני רעבה": getYomKippur}
 
 @client.event
 async def on_ready():
