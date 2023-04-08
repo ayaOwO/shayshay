6770826143e0429ceed6c4d1153fb45fae83e115
ayayawo <tatytoy1@gmail.com>
2023-04-08 20:21:33 +0300

    Added orchestrator

---
+++ orchestrator.py
@@ -0,0 +1,44 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from discord.ext import commands
+
+import discord
+
+from commandInterpreter import CommandInterpreter
+
+
+class Orchestrator():
+    def create_logger(self):
+        logger = logging.Logger("shayshay")
+        formatter = logging.Formatter(fmt="%(asctime)s :: %(levelname)s :: %(message)s")
+        handler = RotatingFileHandler("./logs/shayshay.log", backupCount=10)
+        handler.formatter = formatter
+        logger.addHandler(handler)
+        logging.Formatter = formatter
+
+        return logger
+
+    def create_token(self):
+        with open("bot.token") as token_file:
+            token = token_file.read()
+        print(f'token is {token}')
+        return token
+
+    def create_command_interpreter(self, help_file_name):
+        return CommandInterpreter(help_file_name)
+
+    def create_intents(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        return intents
+
+    def create_bot(self, pre):
+        intents = self.create_intents()
+        bot = commands.Bot(command_prefix=pre, intents=intents)
+
+        return bot
+
+    def create_client(self):
+        intents = self.create_intents()
+        return discord.Client(intents=intents)
--- shayshayBot.py
+++ shayshayBot.py
@@ -1,21 +1,20 @@
+from orchestrator import Orchestrator
+
 import discord
 from discord import app_commands
-from discord.ext import commands
 from datetime import datetime
-from commandInterpreter import CommandInterpreter
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
+orchestrator = Orchestrator()
+help_file_name = "help.txt"
+command_interpreter = orchestrator.create_command_interpreter(help_file_name)
+logger = orchestrator.create_logger()
+pre = "שישי"
+bot = orchestrator.create_bot(pre)
+client = orchestrator.create_client()
+tree = app_commands.CommandTree(client)
 
 # functions
-server_id = 843477859020308510
-pre = "שישי"
-help_file_name = "help.txt"
-command_interpreter = CommandInterpreter(help_file_name)
-bot = commands.Bot(command_prefix=pre, intents=intents)
-tree = app_commands.CommandTree(client)
+server_id = 1040923447188004864
 
 
 @tree.command(name="שבת", description="Tells you when Avishay comes back, so the grind can continue",
@@ -50,8 +49,5 @@ async def on_message(message):
 
 
 # running bot
-token_file = open("bot.token")
-token = token_file.read()
-token_file.close()
-print(f'token is {token}')
+token = orchestrator.create_token()
 client.run(token)
